88acdb6466780f57f7f2595a8c0fb86f576eb62e
Shaya <suarezshaya@gmail.com>
2022-06-20 22:30:13 -0300

    modificaciones

---
+++ pinkaapp/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar= () => {
+return (
+<ul className="nav">
+<li>PINKA</li>
+<li> <a href="/index.html">Inicio</a></li>
+<li><a href="./contacto.html">Contacto</a></li>
+</ul>
+)
+}
+export default NavBar
