2a5fa937943a8edaaeee4932e3e7a0f59493197c
Shaya <suarezshaya@gmail.com>
2022-06-27 14:34:50 -0300

    Modificaciones

--- pinkaapp/src/containers/ItemListContainer/index.jsx
+++ pinkaapp/src/containers/ItemListContainer/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ItemCount from '../../components/ItemCount';
 
 const ItemListContainer = ({greeting}) => {
+    const handleAdd = () => {
+        console.log("Se agrego al carrito")
+    }
     return (
        <div>
         <h2> {greeting}</h2>
+        <ItemCount handleAdd={handleAdd}/>
        </div> 
     )
 }
