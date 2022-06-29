7419fdd99d9936fd3e1d1d17589aeae535cd2e5c
Shaya <suarezshaya@gmail.com>
2022-06-28 21:06:39 -0300

    Item Count

--- pinkaapp/src/components/ItemCount/index.jsx
+++ pinkaapp/src/components/ItemCount/index.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import { useState } from 'react';
+import './itemCount.css';
 
-const ItemCount= ({handleAdd}) => {
-const [count, setCount] = useState (0)
-const onAdd = () => {
-}
+const ItemCount= ({onAdd}) => {
+const inicial=1;
+const stock= 5;
+const [qty, setQty] = useState(inicial);
+const addProduct = (num) => {
+  setQty(qty + num);
+};
+return (
+  <div className="count-container">
+    <div className="count-contador">
+      <button
+        className="count-button"
+        onClick={() => addProduct(-1)}
+        disabled={qty === inicial ? true : null}
+        >
+          -
+        </button>
+        <span className="count-container__qty">{qty}</span>
+        <button
+          className="count-button"
+          onClick={() => addProduct(+1)}
+         disabled={qty === stock ? true : null}
+          >
+            +
+          </button>
+        </div>
+        <button
+        className="button-primary"
+        onClick={() => onAdd(qty)}
+        disabled={stock === 0 ? true : null}
+      >
+        Agregar al carrito
+      </button>
+    </div>
+  );
+};
 
-const onDecrement = () => {
+export default ItemCount;
+
+
+
+/*</div>const onDecrement = () => {
 }
 return (
     <div>
@@ -17,5 +54,4 @@ return (
 
   </div>
 )
-}
-export default ItemCount
+}*/
